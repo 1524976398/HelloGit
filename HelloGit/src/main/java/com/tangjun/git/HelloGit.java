30d0a4b51859f5d9307dde59a9b332356209d8c9
xiaoming <xm@sina.com>
2020-09-21 12:23:23 +0800

    更新一

--- HelloGit/src/main/java/com/tangjun/git/HelloGit.java
+++ HelloGit/src/main/java/com/tangjun/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.tangjun.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello 更新一!");
     }
 }
